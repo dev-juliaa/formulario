8c3f0936e5881ea91e550a99d0b90e205d5d7428
juliaAraujodev <63320916+szya5@users.noreply.github.com>
2022-10-17 19:34:01 -0300

    Update index.js

--- SCRIPT/index.js
+++ SCRIPT/index.js
@@ -3,9 +3,6 @@ const username = document.getElementById("username");
 const email = document.getElementById("email");
 const password = document.getElementById("password");
 
-const telefone = document.getElementById('telefone');
-const cep = document.getElementById('cep');
-
 form.addEventListener("submit", (e) => {
   e.preventDefault();
 
@@ -17,52 +14,25 @@ function checkInputs() {
   const emailValue = email.value;
   const passwordValue = password.value;
 
-
   if (usernameValue === "") {
-    setErrorFor("O nome de usuário é obrigatório.");
+    setErrorFor(username, "O nome de usuário é obrigatório.");
   } else {
     setSuccessFor(username);
   }
 
-  if (emailValue === "") { 
-    setErrorFor("Por favor, insira um email válido.");
-  } else {
-    setSuccessFor(email);
-  }
-
-  if (telefone === "") {
-  } else if (telefone.length <8) {
-    setErrorFor("Por favor, insira um telefone válido.");
-  } else {
-    setSuccessFor(telefone);
-  }
-
-  if (cep === "") {
-    setErrorFor( "O cep é obrigatório.");
-  } else if (cep.length <7) {
-    setErrorFor("Por favor, insira um cep válido.");
-  } else {
-    setSuccessFor(cep);
-  }
+  if (emailValue === "") {
+    alert("O email é obrigatório.");
+  } else (!checkEmail(emailValue)) {
+    alert("Por favor, insira um email válido.");
+  } 
 
   if (passwordValue === "") {
-    setErrorFor("A senha é obrigatória.");
+    setErrorFor(password, "A senha é obrigatória.");
   } else if (passwordValue.length < 7) {
-    setErrorFor("A senha precisa ter no mínimo 7 caracteres.");
+    setErrorFor(password, "A senha precisa ter no mínimo 7 caracteres.");
   } else {
     setSuccessFor(password);
   }
-
-  const formControls = form.querySelectorAll(".form-control");
-
-  const formIsValid = [...formControls].every((formControl) => {
-    return formControl.className === "form-control success";
-  });
-
-  if (formIsValid) {
-    console.log("O formulário está 100% válido!");
-  }
-}
 
 function setErrorFor(input, message) {
   const formControl = input.parentElement;
@@ -78,9 +48,6 @@ function setErrorFor(input, message) {
 function setSuccessFor(input) {
   const formControl = input.parentElement;
 
-  // Adicionar a classe de sucesso
-  formControl.className = "form-control success";
-}
 
 function checkEmail(email) {
   return /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(
